074f1d3d34448b94d3ef505b61614532a9b4249a
omerraza313 <omerraza313@gmail.com>
2019-11-25 21:48:09 +0500

    img solve

--- resources/views/Admin/Pages/dashboard.blade.php
+++ resources/views/Admin/Pages/dashboard.blade.php
@@ -357,7 +357,7 @@
                           <span class="direct-chat-timestamp float-right">23 Jan 2:00 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('dist/img/user1-128x128.jpg')}}" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           Is this template really for free? That's unbelievable!
@@ -373,7 +373,7 @@
                           <span class="direct-chat-timestamp float-left">23 Jan 2:05 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('dist/img/user3-128x128.jpg')}}" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           You better believe it!
@@ -389,7 +389,7 @@
                           <span class="direct-chat-timestamp float-right">23 Jan 5:37 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="dist/img/user1-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('dist/img/user1-128x128.jpg')}}" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           Working with AdminLTE on a great new app! Wanna join?
@@ -405,7 +405,7 @@
                           <span class="direct-chat-timestamp float-left">23 Jan 6:10 pm</span>
                         </div>
                         <!-- /.direct-chat-infos -->
-                        <img class="direct-chat-img" src="dist/img/user3-128x128.jpg" alt="message user image">
+                        <img class="direct-chat-img" src="{{ asset('dist/img/user3-128x128.jpg')}}" alt="message user image">
                         <!-- /.direct-chat-img -->
                         <div class="direct-chat-text">
                           I would love to.
@@ -422,7 +422,7 @@
                       <ul class="contacts-list">
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="dist/img/user1-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('dist/img/user1-128x128.jpg')}}">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -437,7 +437,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="dist/img/user7-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('dist/img/user7-128x128.jpg')}}">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -482,7 +482,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="dist/img/user6-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('dist/img/user6-128x128.jpg')}}">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -497,7 +497,7 @@
                         <!-- End Contact Item -->
                         <li>
                           <a href="#">
-                            <img class="contacts-list-img" src="dist/img/user8-128x128.jpg">
+                            <img class="contacts-list-img" src="{{ asset('dist/img/user8-128x128.jpg')}}">
 
                             <div class="contacts-list-info">
                               <span class="contacts-list-name">
@@ -550,17 +550,17 @@
                   <div class="card-body p-0">
                     <ul class="users-list clearfix">
                       <li>
-                        <img src="dist/img/user1-128x128.jpg" alt="User Image">
+                        <img src="{{ asset('dist/img/user1-128x128.jpg')}}" alt="User Image">
                         <a class="users-list-name" href="#">Alexander Pierce</a>
                         <span class="users-list-date">Today</span>
                       </li>
                       <li>
-                        <img src="dist/img/user8-128x128.jpg" alt="User Image">
+                        <img src="{{('dist/img/user8-128x128.jpg')}}" alt="User Image">
                         <a class="users-list-name" href="#">Norman</a>
                         <span class="users-list-date">Yesterday</span>
                       </li>
                       <li>
-                        <img src="dist/img/user7-128x128.jpg" alt="User Image">
+                        <img src="{{('dist/img/user7-128x128.jpg')}}" alt="User Image">
                         <a class="users-list-name" href="#">Jane</a>
                         <span class="users-list-date">12 Jan</span>
                       </li>
